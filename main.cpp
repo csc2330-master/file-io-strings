aa60333626e26e6665e640130c326f0c17bb616c
Carlos R. Arias <arias@spu.edu>
2019-10-23 10:11:16 -0700

    It's counting words per line, and words per file.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <iomanip>
 
 using std::cout;
 using std::getline;
@@ -9,18 +10,40 @@ using std::endl;
 using std::cerr;
 using std::ifstream;
 using std::string;
+using std::setw;
+using std::left;
+using std::right;
 
 
-
+// How can we count words?
+// How can we count hoe many "of" inside that file?
+// How to add all the different words inside a vector?
 
 int main(int argc, char* argv[]) {
     string line;
     ifstream inputFile("data.txt");
-    int lines = 0;
+    int lines = 0, from, words, pos, allWords = 0;
     while (getline(inputFile, line)){
-        cout << ++lines << "\t" << line << endl;
+        words = 1;
+        from = 0;
+        pos = line.find(" ", from);
+        while (pos != string::npos){
+            words++;
+            from = pos + 1;
+            pos = line.find(" ", from);
+        }
+
+        allWords += words;
+        cout << setw(3) << ++lines << " " << left << setw(55) << line << right << setw(3) << words << endl;
+//        for (int i = 0; i < line.length(); ++i) {
+//            cout << line[i] << "\t";
+//        }
+        //cout << endl;
     }
     inputFile.close();
+    cout << "File has: " << endl
+         << "Lines: " << lines << endl
+         << "Words: " << allWords << endl << endl;
 
     return 0;
 }
